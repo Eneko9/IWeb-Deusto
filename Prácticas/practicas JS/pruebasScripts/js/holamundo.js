0a21c7d6faefae5140536a6bbaf18da0f762d4d9
Eneko9 <eneko.eguiguren@opendeusto.es>
2021-11-30 08:58:58 +0100

    todas las practicas de la asignatura

---
+++ Prácticas/practicas JS/pruebasScripts/js/holamundo.js
@@ -0,0 +1,15 @@
+//Introduciendo scripts para practicar
+function saludo(){
+    alert('Hola mundo!');
+}
+
+//sacando por consola el numero de elementos de tipo a
+let elementos_a = document.body.querySelectorAll('a');
+console.log(elementos_a.length);
+
+//sacando por consola el contenido de el id='articulo_sidebar'
+let articuloSb = document.getElementById('articulo_sidebar');
+console.log(articuloSb);
+
+
+
